c77af91bba23f5a3f4bae4234b5e1fcdc8ac1834
Adnan Irteja <adnanirteja10@gmail.com>
2026-09-05 11:56:45 +0600

    Remove old development files

--- dump/couponfix1.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE coupons
-ADD CONSTRAINT unique_restaurant_coupon
-UNIQUE (restaurant_id, coupon_code);
--- dump/restaurantAndCustomerFix1.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE customers
-ADD CONSTRAINT fk_customers_user
-FOREIGN KEY (login_id)
-REFERENCES users(id);
-
-ALTER TABLE restaurants
-ADD CONSTRAINT fk_restaurants_user
-FOREIGN KEY (login_id)
-REFERENCES users(id);
